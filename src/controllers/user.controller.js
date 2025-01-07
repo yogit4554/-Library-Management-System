9125f06a9779a34570c2a2b57a4fd84018dd8433
yogit4554 <yt7407286@gmail.com>
2025-01-07 15:34:59 +0530

    added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,6 +30,7 @@ const loginUser = asyncHandler(async (req, res) => {
                 _id: admin._id,
                 name: admin.name,
                 email: admin.email,
+                role:admin.role,
                 token,
             });
         } else {
